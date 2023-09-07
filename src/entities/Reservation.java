46bd0f0b5fd5e1828842402defb971e69c0178d3
Matheus Eler <matheus.eler@outlook.com>
2023-09-07 13:29:25 -0300

    projeto atualizado

--- src/entities/Reservation.java
+++ src/entities/Reservation.java
@@ -1,10 +1,66 @@
 package entities;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Reservation {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private Integer roomNumber;
+	private Date checkIn;
+	private Date checkOut;
+	
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public Reservation () {
+		
+		
 	}
+
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+		this.roomNumber = roomNumber;
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+
+	public Integer getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(Integer roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public Date getCheckIn() {
+		return checkIn;
+	}
+
+	public Date getCheckOut() {
+		return checkOut;
+	}
+
+	public long durantion() {
+		long dif = checkOut.getTime() - checkOut.getTime();
+		return TimeUnit.DAYS.convert(dif, TimeUnit.MILLISECONDS);
+			
+	}
+	
+	public void updates (Date checkIn, Date checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "ROOM:" + roomNumber 
+				+ ",checkIn: " + sdf.format(checkIn)
+				+ ",checkOut: " +sdf.format(checkOut) 
+				+"duration: " +durantion();
+				
+	}
+	
+	
 
 }
